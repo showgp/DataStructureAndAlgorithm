7103025134912efe2c515d35eb90ac0c42a1d3d2
ray <showgpray@gmail.com>
2019-11-15 12:50:34 +0800

    数学模型初步.

--- Src/DSAlgo/C1/AlgoAnalysis/ThreeSum.cs
+++ Src/DSAlgo/C1/AlgoAnalysis/ThreeSum.cs
@@ -5,10 +5,14 @@ namespace C1.AlgoAnalysis {
         public static int Count(int[] a) {
             int N = a.Length;
             int cnt = 0;
+            UInt64 total = 0;
             for (int i = 0; i < N; i++)
                 for (int j = i + 1; j < N; j++)
-                    for (int k = j + 1; k < N; k++)
+                    for (int k = j + 1; k < N; k++) {
+                        total++;
                         if (a[i] + a[j] + a[k] == 0) cnt++;
+                    }
+            System.Console.WriteLine($"total execution times: {total}");
             return cnt;
         }
 
