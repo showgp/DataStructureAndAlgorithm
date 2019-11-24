93a6e9b6c65f1e799c13ef15eaf381c62d5fcae5
ray <showgp@163.com>
2019-11-24 17:05:34 +0800

    feat: 展开链式队列, 暂未实现.

---
+++ Src/DSAlgo/C1/Implementations/LinkedQueue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using C1.Interfaces;
+
+namespace C1.Implementations {
+
+    public class LinkedQueue<Item>: ILinkedQueue<Item> {
+        private ISingleLinkedList<Item> _list;
+
+        public LinkedQueue() {
+            _list = new SingleLinkedList<Item>();
+        }
+
+        public Item Dequeue() {
+            throw new NotImplementedException();
+        }
+
+        public void Enqueue(Item item) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Item> GetEnumerator() {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty() {
+            throw new NotImplementedException();
+        }
+
+        public int Size() {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            throw new NotImplementedException();
+        }
+    }
+}
