eeb2d1cde4e103092c7ed7935661b1bd70d929e5
ray <showgpray@gmail.com>
2019-11-24 16:05:52 +0800

    feat: 单链表实现及单元测试.

--- Src/DSAlgo/C1/Implementations/SingleLinkedList.cs
+++ Src/DSAlgo/C1/Implementations/SingleLinkedList.cs
@@ -12,32 +12,51 @@ namespace C1.Implementations {
 
         public SingleLinkedList() {
             _head = new SingleLinkedListNode<Item>(default, null);
-            _last = null;
+            _last = _head;
         }
 
         public void InsertAtEnd(Item item) {
-            throw new System.NotImplementedException();
+            var node = new SingleLinkedListNode<Item>(item, null);
+            _last.Next = node;
+            _last = node;
+            _size++;
         }
 
         public void InsertAtStart(Item item) {
             var node = new SingleLinkedListNode<Item>(item, _head.Next);
             _head.Next = node;
             _size++;
-            if (_last == null) { // 空表插入的情况
+            if (_last == _head) { // 空表插入的情况
                 _last = node;
             }
         }
 
         public Item RemoveFromEnd() {
-            throw new System.NotImplementedException();
+            if (IsEmpty()) return default;
+            var preLast = _head;
+            while (preLast.Next != _last) {
+                preLast = preLast.Next;
+            }
+            var lastNode = _last;
+            _last = preLast;
+            _last.Next = null;
+            _size--;
+            return lastNode.Value;
         }
 
         public Item RemoveFromStart() {
-            throw new System.NotImplementedException();
+            if (IsEmpty()) return default;
+            var node = _head.Next;
+            _head.Next = node.Next;
+            _size--;
+            if (_size == 0) {
+                _last = _head;
+            }
+            return node.Value;
         }
 
         public int Size() => _size;
 
-        public bool IsEmpty() => _size <= 0;
+        public bool IsEmpty() => _size == 0;
     }
 }
--- Tests/DSAlgo.Tests/SingleLinkedListTests.cs
+++ Tests/DSAlgo.Tests/SingleLinkedListTests.cs
@@ -25,11 +25,11 @@ namespace DSAlgo.Tests {
         测试:
         1. 构造 √
         1. 头部插入 √
-        1. 头部删除
-        1. 尾部插入
-        1. 尾部删除
-        1. 是否为空
-        1. 元素个数
+        1. 头部删除 √
+        1. 尾部插入 √
+        1. 尾部删除 √
+        1. 是否为空 √
+        1. 元素个数 √
         */
 
         [TestMethod]
@@ -55,6 +55,104 @@ namespace DSAlgo.Tests {
             Assert.AreEqual(1, _sut.Size());
             Assert.IsNotNull(_sut.Last);
             Assert.AreEqual(_sut.Head.Next, _sut.Last);
+        }
+
+        [TestMethod]
+        public void TestDeletionAtStart() {
+            var elem1 = "hello1";
+            var elem2 = "world2";
+            _sut.InsertAtStart(elem1);
+            _sut.InsertAtStart(elem2);
+
+            Assert.IsFalse(_sut.IsEmpty());
+            Assert.AreEqual(2, _sut.Size());
+            Assert.AreEqual(elem2, _sut.Head.Next.Value);
+            Assert.AreEqual(elem1, _sut.Head.Next.Next.Value);
+            Assert.IsNull(_sut.Head.Next.Next.Next);
+
+            var r1 = _sut.RemoveFromStart();
+            Assert.AreEqual(elem2, r1);
+            Assert.AreEqual(1, _sut.Size());
+            Assert.IsFalse(_sut.IsEmpty());
+            var r2 = _sut.RemoveFromStart();
+            Assert.AreEqual(elem1, r2);
+            Assert.AreEqual(0, _sut.Size());
+            Assert.IsTrue(_sut.IsEmpty());
+            Assert.IsNull(_sut.Head.Next);
+            Assert.AreEqual(_sut.Head, _sut.Last);
+
+            var r3 = _sut.RemoveFromStart();
+            Assert.IsNull(r3);
+            Assert.AreEqual(0, _sut.Size());
+            Assert.IsTrue(_sut.IsEmpty());
+            Assert.AreEqual(_sut.Head, _sut.Last);
+        }
+
+        [TestMethod]
+        public void TestInsertOneElemAtEnd() {
+            var elem1 = "hello1";
+            _sut.InsertAtEnd(elem1);
+            Assert.AreEqual(elem1, _sut.Last.Value);
+            Assert.IsNull(_sut.Last.Next);
+            Assert.AreEqual(1, _sut.Size());
+            Assert.IsFalse(_sut.IsEmpty());
+            Assert.AreEqual(_sut.Head.Next, _sut.Last);
+        }
+
+        [TestMethod]
+        public void TestInsertMoreThanOneElemAtEnd() {
+            var elem1 = "elem1";
+            var elem2 = "elem2";
+            var elem3 = "elem3";
+            _sut.InsertAtEnd(elem1);
+            _sut.InsertAtEnd(elem2);
+            _sut.InsertAtEnd(elem3);
+            Assert.AreEqual(3, _sut.Size());
+            Assert.IsFalse(_sut.IsEmpty());
+            Assert.AreEqual(elem1, _sut.Head.Next.Value);
+            Assert.AreEqual(elem2, _sut.Head.Next.Next.Value);
+            Assert.AreEqual(elem3, _sut.Head.Next.Next.Next.Value);
+            Assert.AreEqual(elem3, _sut.Last.Value);
+            Assert.IsNull(_sut.Last.Next);
+            Assert.AreNotEqual(_sut.Last, _sut.Head.Next);
+        }
+
+        [TestMethod]
+        public void TestRemoveFromEnd() {
+            var elem1 = "elem1";
+            var elem2 = "elem2";
+            var elem3 = "elem3";
+            _sut.InsertAtEnd(elem1);
+            _sut.InsertAtEnd(elem2);
+            _sut.InsertAtEnd(elem3);
+
+            Assert.AreEqual(3, _sut.Size());
+            Assert.AreEqual(elem1, _sut.Head.Next.Value);
+            Assert.AreEqual(elem3, _sut.Last.Value);
+
+            var r1 = _sut.RemoveFromEnd();
+            Assert.AreEqual(elem3, r1);
+            Assert.AreEqual(2, _sut.Size());
+            Assert.AreEqual(elem2, _sut.Last.Value);
+
+            var r2 = _sut.RemoveFromEnd();
+            Assert.AreEqual(1, _sut.Size());
+            Assert.IsFalse(_sut.IsEmpty());
+            Assert.AreEqual(elem2, r2);
+            Assert.AreEqual(elem1, _sut.Last.Value);
+
+            var r3 = _sut.RemoveFromEnd();
+            Assert.AreEqual(0, _sut.Size());
+            Assert.IsTrue(_sut.IsEmpty());
+            Assert.AreEqual(elem1, r3);
+            Assert.AreEqual(_sut.Head, _sut.Last);
+            Assert.IsNull(_sut.Head.Next);
+
+            var r4 = _sut.RemoveFromEnd();
+            Assert.IsNull(r4);
+            Assert.AreEqual(0, _sut.Size());
+            Assert.AreEqual(_sut.Head, _sut.Last);
+            Assert.IsTrue(_sut.IsEmpty());
         }
     }
 }
